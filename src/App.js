5428ded566f5feb669ccdf8fbbd6e08f9e479b6f
Bhairavnath <bhairavbarad@gmail.com>
2023-10-14 17:07:11 +0530

    new_project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import MyNavbar from './Components/MyNavbar'
+import Home from './pages/Home'
+import "./app.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <>
+  <BrowserRouter>
+  <MyNavbar/>
+  <Routes>
+    <Route path="/" element={<Home/>}>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/signup" element={<SignUp/>}/>
+    </Route>
+  </Routes>
+  </BrowserRouter>
+  </>
 }
 
-export default App;
+export default App
