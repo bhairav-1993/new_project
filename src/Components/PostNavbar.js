ee55a52c01c1c214965fcc78f76ca09f6be720b2
Bhairavnath <bhairavbarad@gmail.com>
2023-10-14 17:49:25 +0530

    gg

--- src/Components/PostNavbar.js
+++ src/Components/PostNavbar.js
@@ -4,16 +4,16 @@ const PostNavbar = () => {
   return <>
   <nav class="navbar navbar-expand-lg bg navbar-light mb-4">
     <div class="container-fluid">
-      <a class="navbar-brand underline" href="#">All Posted (32)</a>
+      <a class="navbar-brand underline" href="#1">All Posted (32)</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <a class="nav-link active" href="#">Artical</a>
-          <a class="nav-link" href="#">Event</a>
-          <a class="nav-link" href="#">Education</a>
-          <a class="nav-link" href="#">Job</a>
+          <a class="nav-link active" href="#4">Artical</a>
+          <a class="nav-link" href="#1">Event</a>
+          <a class="nav-link" href="#2">Education</a>
+          <a class="nav-link" href="#3">Job</a>
         </div>
         <div className="ms-auto d-flex gap-4">
             <div class="dropdown">
@@ -21,9 +21,9 @@ const PostNavbar = () => {
 Write a post
               </button>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="#4">Action</a></li>
+                <li><a class="dropdown-item" href="#5">Another action</a></li>
+                <li><a class="dropdown-item" href="#6">Something else here</a></li>
               </ul>
             </div>
             <button type="button" class="btn btn-primary"><i class="bi bi-people-fill"></i> Join Group</button>
